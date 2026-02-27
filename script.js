b1215330fc7bff7b141fac07c9c789e6cbf0d695
Martinf88 <143597591+Martinf88@users.noreply.github.com>
2026-02-27 14:28:53 +0100

    Changes icons from spans to buttons --Limits max width of main

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ function renderBooks() {
     newTextWrapper.append(newAuthor, newSpacer, newPages);
     newMainWrapper.append(newTitle, newTextWrapper);
 
-    const readIcon = document.createElement("span");
+    const readIcon = document.createElement("button");
     readIcon.classList.toggle("read-icon", book.read);
     readIcon.classList.toggle("not-read-icon", !book.read);
     readIcon.textContent = book.read ? "✅" : "❌";
@@ -68,14 +68,14 @@ function renderBooks() {
     const editWrapper = document.createElement("div");
     editWrapper.classList.add("edit-wrapper");
 
-    const editIcon = document.createElement("div");
+    const editIcon = document.createElement("button");
     editIcon.classList.add("edit-icon");
     editIcon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
 </svg>
 `;
 
-    const deleteIcon = document.createElement("div");
+    const deleteIcon = document.createElement("button");
     deleteIcon.classList.add("delete-icon");
     deleteIcon.innerHTML = `
       <svg
