7a13cbb70d4b80c3ff4ee4bc9f07d3793a90450f
Martinf88 <143597591+Martinf88@users.noreply.github.com>
2026-03-02 10:21:45 +0100

    Adds delete- and toggle read functionality -- Removes editBtn. Dont intend to implement that functionality -- Updates readBtn icon when toggling -- Updates hover effect on buttons for better user experience -- Updates naming for created elements for readability purposes -- Adds variables for SVG's for better readability

--- script.js
+++ script.js
@@ -2,6 +2,25 @@ const librarySection = document.getElementById("library-section");
 const bookDialog = document.getElementById("book-dialog");
 const bookForm = document.getElementById("book-form");
 
+const DELETE_ICON_SVG = `
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="size-6"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+        />
+      </svg>
+    `;
+const READ_ICON_SVG = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-book-open-check-icon lucide-book-open-check"><path d="M12 21V7"/><path d="m16 12 2 2 4-4"/><path d="M22 6V4a1 1 0 0 0-1-1h-5a4 4 0 0 0-4 4 4 4 0 0 0-4-4H3a1 1 0 0 0-1 1v13a1 1 0 0 0 1 1h6a3 3 0 0 1 3 3 3 3 0 0 1 3-3h6a1 1 0 0 0 1-1v-1.3"/></svg>`;
+const NOT_READ_ICON_SVG = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-book-open-icon lucide-book-open"><path d="M12 7v14"/><path d="M3 18a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h5a4 4 0 0 1 4 4 4 4 0 0 1 4-4h5a1 1 0 0 1 1 1v13a1 1 0 0 1-1 1h-6a3 3 0 0 0-3 3 3 3 0 0 0-3-3z"/></svg>`;
+
 function Book(title, author, pages, read) {
   if (!new.target) {
     throw new Error(
@@ -15,6 +34,10 @@ function Book(title, author, pages, read) {
   this.pages = pages;
   this.read = Boolean(read);
 }
+
+Book.prototype.toggleRead = function () {
+  this.read = !this.read;
+};
 
 const myLibrary = [];
 
@@ -33,72 +56,64 @@ function addBookToLibrary(title, author, pages, read) {
 }
 
 function createBookCard(book) {
-  const newDiv = document.createElement("div");
-  newDiv.classList.add("card");
+  const bookCard = document.createElement("div");
+  bookCard.classList.add("card");
 
-  const newMainWrapper = document.createElement("div");
-  newMainWrapper.classList.add("main-wrapper");
+  const mainWrapper = document.createElement("div");
+  mainWrapper.classList.add("main-wrapper");
 
-  const newTitle = document.createElement("h2");
-  newTitle.classList.add("title");
-  newTitle.innerHTML = book.title;
+  const bookTitle = document.createElement("h2");
+  bookTitle.classList.add("title");
+  bookTitle.innerHTML = book.title;
 
-  const newTextWrapper = document.createElement("div");
-  newTextWrapper.classList.add("text-wrapper");
+  const textWrapper = document.createElement("div");
+  textWrapper.classList.add("text-wrapper");
 
-  const newAuthor = document.createElement("p");
-  newAuthor.innerHTML = book.author;
-  newAuthor.classList.add("author");
+  const author = document.createElement("p");
+  author.innerHTML = book.author;
+  author.classList.add("author");
 
-  const newSpacer = document.createElement("p");
-  newSpacer.innerHTML = "-";
+  const textSpacer = document.createElement("p");
+  textSpacer.innerHTML = "-";
 
-  const newPages = document.createElement("p");
-  newPages.classList.add("pages");
-  newPages.innerHTML = book.pages;
+  const pages = document.createElement("p");
+  pages.classList.add("pages");
+  pages.innerHTML = book.pages;
 
-  newTextWrapper.append(newAuthor, newSpacer, newPages);
-  newMainWrapper.append(newTitle, newTextWrapper);
+  textWrapper.append(author, textSpacer, pages);
+  mainWrapper.append(bookTitle, textWrapper);
 
-  const readIcon = document.createElement("button");
-  readIcon.classList.toggle("read-icon", book.read);
-  readIcon.classList.toggle("not-read-icon", !book.read);
-  readIcon.textContent = book.read ? "✅" : "❌";
+  const readBtn = document.createElement("button");
+  readBtn.classList.toggle("read-icon", book.read);
+  readBtn.classList.toggle("not-read-icon", !book.read);
+  readBtn.innerHTML = book.read ? READ_ICON_SVG : NOT_READ_ICON_SVG;
+  readBtn.addEventListener("click", () => {
+    book.toggleRead();
+    updateReadButton(readBtn, book);
+  });
 
-  const editWrapper = document.createElement("div");
-  editWrapper.classList.add("edit-wrapper");
+  const deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("delete-icon");
+  deleteBtn.innerHTML = DELETE_ICON_SVG;
 
-  const editIcon = document.createElement("button");
-  editIcon.classList.add("edit-icon");
-  editIcon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
-</svg>
-`;
+  deleteBtn.addEventListener("click", () => deleteBook(book, bookCard));
 
-  const deleteIcon = document.createElement("button");
-  deleteIcon.classList.add("delete-icon");
-  deleteIcon.innerHTML = `
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke="currentColor"
-        class="size-6"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-        />
-      </svg>
-    `;
+  bookCard.append(readBtn, mainWrapper, deleteBtn);
 
-  editWrapper.append(editIcon, deleteIcon);
+  librarySection.appendChild(bookCard);
+}
 
-  newDiv.append(readIcon, newMainWrapper, editWrapper);
+function updateReadButton(button, book) {
+  button.classList.toggle("read-icon", book.read);
+  button.classList.toggle("not-read-icon", !book.read);
+  button.innerHTML = book.read ? READ_ICON_SVG : NOT_READ_ICON_SVG;
+}
 
-  librarySection.appendChild(newDiv);
+function deleteBook(book, bookCard) {
+  const index = myLibrary.findIndex((b) => b.id === book.id);
+  myLibrary.splice(index, 1);
+
+  bookCard.remove();
 }
 
 function renderBooks() {
